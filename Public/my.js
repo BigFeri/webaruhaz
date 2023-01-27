1dae57d7d92cc2a9bd2aabb83a9efc5dfdf221bf
Ferenc <ferenc0516@gmail.com>
2023-01-27 12:08:31 +0100

    javítás

--- Public/my.js
+++ Public/my.js
@@ -1,1 +1,24 @@
 console.log("Hello");
+const url = "http://localhost:3000/products/"
+
+
+async function getProducts() {
+    let respone = await fetch(url);
+    let data = await response.json()
+    console.log(data);
+}
+
+async function getProducts(){
+    let body = {
+        name: "Á6",
+        quantity: 66,
+        price: 6500,
+        type: "fémáru"
+    }
+
+    let response = awit fetch(url, {
+        Method:
+    })
+}
+
+getProducts();
